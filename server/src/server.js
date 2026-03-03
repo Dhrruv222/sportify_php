af7298a88785b5654cd44063b027d9d5c1e1a868
Dhrruv222 <dhrruv91@gmail.com>
2026-03-03 22:07:37 +0100

    feat(server): add scheduled locale-based news enqueue flow

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 const app = require('./app');
 const { startNewsIngestionWorker } = require('./modules/news/news.queue');
+const { startAutoNewsIngestionScheduler } = require('./modules/news/news.scheduler');
 
 const PORT = process.env.PORT || 3000;
 
 startNewsIngestionWorker();
+startAutoNewsIngestionScheduler();
 
 app.listen(PORT, () => {
   console.log(`Running in port ${PORT}`);
